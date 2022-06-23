70e68c82e468e23fffca68c1be23cad8521b73b5
Eduardo Santos <eduardosantos.ist@gmail.com>
2022-06-23 11:38:39 -0300

    Setup projeto Ignite Labs

---
+++ src/lib/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: "https://api-sa-east-1.graphcms.com/v2/cl4q42frs2pmj01xn48l71mkk/master",
+  cache: new InMemoryCache(),
+});
